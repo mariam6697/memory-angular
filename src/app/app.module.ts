bafa7880d481e4a8cfd0a7cf5246e062ea1e17b3
mariam6697 <mariam.maldonado.marin@gmail.com>
2023-08-14 18:36:37 -0400

    feat: adding tailwind and removing bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgbModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
