2fb0f9191a74cf2727d7cc4b9b0bba88485e04ed
mariam6697 <mariam.maldonado.marin@gmail.com>
2023-08-14 20:03:57 -0400

    feat: hidden images and flip card function

--- src/app/components/image-card/image-card.component.ts
+++ src/app/components/image-card/image-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import ICONS from 'src/app/constants/icons.constants';
 import { IGameCard } from 'src/app/models/game-card.model';
 
 @Component({
@@ -8,8 +10,21 @@ import { IGameCard } from 'src/app/models/game-card.model';
 })
 export class ImageCardComponent implements OnInit {
   @Input() gameCardInfo: IGameCard = {} as IGameCard;
+  public showImage: boolean;
+
+  constructor(private readonly sanitizer: DomSanitizer) {
+    this.showImage = false;
+  }
 
   ngOnInit(): void {
     console.log('gameCardInfo', this.gameCardInfo);
+  }
+
+  clickCard(): void {
+    this.showImage = !this.showImage;
+  }
+
+  get questionIcon(): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(`${ICONS.QUESTION}`);
   }
 }
