d96f43cf7c2176d3e59f59ee24d836dc80586012
mariam6697 <mariam.maldonado.marin@gmail.com>
2023-08-15 20:42:50 -0400

    test: adding unit tests for ToastService

--- src/app/services/toast.service.spec.ts
+++ src/app/services/toast.service.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ToastService } from './toast.service';
+import { ToastInfo } from '../models/toast.model';
+
+const testToast = {} as ToastInfo;
 
 describe('ToastService', () => {
   let service: ToastService;
@@ -12,5 +15,23 @@ describe('ToastService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add toast to array', () => {
+    service.toasts = [];
+    service.show('test');
+    expect(service.toasts.length).toBeGreaterThan(0);
+  });
+
+  it('should remove toast from array', () => {
+    service.toasts = [testToast];
+    service.remove(testToast);
+    expect(service.toasts.length).toBe(0);
+  });
+
+  it('should clear all toasts', () => {
+    service.toasts = [testToast];
+    service.clear();
+    expect(service.toasts.length).toBe(0);
   });
 });
